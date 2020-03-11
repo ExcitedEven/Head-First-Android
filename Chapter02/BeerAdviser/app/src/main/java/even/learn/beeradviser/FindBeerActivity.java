78dfdc64241fec0aed8200507ac013a316a7a7db
ExcitedEven <excitedeven@foxmail.com>
2020-03-11 11:36:40 +0800

    Chapter 02 ends

--- Chapter02/BeerAdviser/app/src/main/java/even/learn/beeradviser/FindBeerActivity.java
+++ Chapter02/BeerAdviser/app/src/main/java/even/learn/beeradviser/FindBeerActivity.java
@@ -1,14 +1,31 @@
 package even.learn.beeradviser;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
 
-public class FindBeerActivity extends AppCompatActivity {
+import java.util.List;
+
+public class FindBeerActivity extends Activity {
+    private BeerExpert expert = new BeerExpert();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_beer);
+    }
+
+    public void onClickFindBeer(View view) {
+        TextView brands = (TextView) findViewById(R.id.brands);
+        Spinner color = (Spinner) findViewById(R.id.color);
+        String beerType = String.valueOf(color.getSelectedItem());
+        List<String> brandlist = expert.getBrands(beerType);
+        StringBuilder brandsFormatted = new StringBuilder();
+        for (String brand : brandlist) {
+            brandsFormatted.append(brand).append("\n");
+        }
+        brands.setText(brandsFormatted);
     }
 }
