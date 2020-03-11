aefb4777613a3cd44c6aa5d1a84ec5f83b9692e9
ExcitedEven <excitedeven@foxmail.com>
2020-03-11 22:19:43 +0800

    daily

--- Chapeter04/Stopwatch/app/src/main/java/even/learn/stopwatch/StopwatchActivity.java
+++ Chapeter04/Stopwatch/app/src/main/java/even/learn/stopwatch/StopwatchActivity.java
@@ -1,14 +1,55 @@
 package even.learn.stopwatch;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.TextView;
 
-public class StopwatchActivity extends AppCompatActivity {
+import java.util.Locale;
+
+public class StopwatchActivity extends Activity {
+
+    private int seconds = 0;
+    private boolean isRunning = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stopwatch);
+        runTimer();
+    }
+
+    public void onClickStart(View view) {
+        isRunning = true;
+        runTimer();
+    }
+
+    public void onClickStop(View view) {
+        isRunning = false;
+    }
+
+    public void onClickReset(View view) {
+        isRunning = false;
+        seconds = 0;
+    }
+
+    private void runTimer() {
+        final TextView timeView = (TextView) findViewById(R.id.time_view);
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int hours = seconds / 3600;
+                int minutees = (seconds % 3600) / 60;
+                int secs = seconds % 60;
+                String time = String.format(Locale.getDefault(), "%d:%02d:%02d", hours, minutees, secs);
+                timeView.setText(time);
+                if (isRunning) {
+                    seconds++;
+                }
+                handler.postDelayed(this, 1000);
+            }
+        });
     }
 }
