b51f80e9a50a1723182298ba578ae683f4f35696
ExcitedEven <excitedeven@foxmail.com>
2020-03-11 19:52:28 +0800

    Create Chapter04

---
+++ Chapeter04/Stopwatch/app/src/main/java/even/learn/stopwatch/StopwatchActivity.java
@@ -0,0 +1,14 @@
+package even.learn.stopwatch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StopwatchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stopwatch);
+    }
+}
