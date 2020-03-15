6364859f98e876952d150f0f7c14b9ea4ad8698a
ExcitedEven <excitedeven@foxmail.com>
2020-03-15 18:01:35 +0800

    TopLevelActivity

---
+++ Chapter07/Starbuzz/app/src/main/java/even/learn/starbuzz/Drink.java
@@ -0,0 +1,39 @@
+package even.learn.starbuzz;
+
+import androidx.annotation.NonNull;
+
+public class Drink {
+    private String name;
+    private String description;
+    private int imageResourceId;
+
+    public static final Drink[] drinks = {
+            new Drink("Latte", "A couple of espresso shots with Steamed milk", R.drawable.latte),
+            new Drink("Cappuccino", "Espresso, hot milk, and a steamed milk foam", R.drawable.cappuccino),
+            new Drink("Filter", "Highest quality beans roasted and brewed fresh", R.drawable.filter)
+    };
+
+    private Drink(String name, String description, int imageResourceId) {
+        this.name = name;
+        this.description = description;
+        this.imageResourceId = imageResourceId;
+    }
+
+    public int getImageResourceId() {
+        return imageResourceId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return this.name;
+    }
+}
