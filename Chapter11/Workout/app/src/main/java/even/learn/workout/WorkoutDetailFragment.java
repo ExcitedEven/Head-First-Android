a2c44dab78b9c5d8339107dfa2a7f9fb31ea0703
ExcitedEven <excitedeven@foxmail.com>
2020-04-19 11:53:46 +0800

    Chapter11 ends

--- Chapter11/Workout/app/src/main/java/even/learn/workout/WorkoutDetailFragment.java
+++ Chapter11/Workout/app/src/main/java/even/learn/workout/WorkoutDetailFragment.java
@@ -5,54 +5,62 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class WorkoutDetailFragment extends Fragment {
-    private long workoutId;
 
-    public WorkoutDetailFragment() {
-    }
+  private long workoutId;
 
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (savedInstanceState != null) {
-            workoutId = savedInstanceState.getLong("workoutId");
-        }
-    }
+  public WorkoutDetailFragment() {
+  }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_workout_detail, container, false);
+  @Override
+  public void onCreate(@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    if (savedInstanceState == null) {
+      StopwatchFragment stopwatch = new StopwatchFragment();
+      FragmentTransaction ft = getChildFragmentManager().beginTransaction();
+      ft.add(R.id.stopwatch_container, stopwatch);
+      ft.addToBackStack(null);
+      ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+      ft.commit();
+    } else {
+      workoutId = savedInstanceState.getLong("workoutId");
     }
+  }
 
-    @Override
-    public void onStart() {
-        super.onStart();
-        View view = getView();
-        if (view != null) {
-            TextView title = (TextView) view.findViewById(R.id.textTitle);
-            Workout workout = Workout.workouts[(int) workoutId];
-            title.setText(workout.getName());
-            TextView description = (TextView) view.findViewById(R.id.textDescription);
-            description.setText(workout.getDescription());
-        }
-    }
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container,
+      Bundle savedInstanceState) {
+    return inflater.inflate(R.layout.fragment_workout_detail, container, false);
+  }
 
-    public void setWorkoutId(long id) {
-        this.workoutId = id;
+  @Override
+  public void onStart() {
+    super.onStart();
+    View view = getView();
+    if (view != null) {
+      TextView title = (TextView) view.findViewById(R.id.textTitle);
+      Workout workout = Workout.workouts[(int) workoutId];
+      title.setText(workout.getName());
+      TextView description = (TextView) view.findViewById(R.id.textDescription);
+      description.setText(workout.getDescription());
     }
+  }
 
-    @Override
-    public void onSaveInstanceState(@NonNull Bundle savedInstanceStace) {
-        savedInstanceStace.putLong("workoutId", workoutId);
-    }
+  public void setWorkoutId(long id) {
+    this.workoutId = id;
+  }
+
+  @Override
+  public void onSaveInstanceState(@NonNull Bundle savedInstanceStace) {
+    savedInstanceStace.putLong("workoutId", workoutId);
+  }
 }
