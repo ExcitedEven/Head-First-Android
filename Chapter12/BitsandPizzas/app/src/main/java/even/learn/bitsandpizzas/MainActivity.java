4358f6e9018bb655126abf7a14235d1bc3c7d404
ExcitedEven <excitedeven@foxmail.com>
2020-04-20 16:53:42 +0800

    标签页

--- Chapter12/BitsandPizzas/app/src/main/java/even/learn/bitsandpizzas/MainActivity.java
+++ Chapter12/BitsandPizzas/app/src/main/java/even/learn/bitsandpizzas/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.ShareActionProvider;
 import androidx.appcompat.widget.Toolbar;
@@ -14,6 +15,7 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.viewpager.widget.ViewPager;
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,6 +31,9 @@ public class MainActivity extends AppCompatActivity {
     SectionsPagerAdapter pagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
     ViewPager pager = (ViewPager) findViewById(R.id.pager);
     pager.setAdapter(pagerAdapter);
+
+    TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+    tabLayout.setupWithViewPager(pager);
   }
 
   @Override
@@ -83,6 +88,22 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public int getCount() {
       return 4;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+      switch (position) {
+        case 0:
+          return getResources().getText(R.string.home_tab);
+        case 1:
+          return getResources().getText(R.string.pizza_tab);
+        case 2:
+          return getResources().getText(R.string.pasta_tab);
+        case 3:
+          return getResources().getText(R.string.store_tab);
+      }
+      return null;
     }
   }
 }
